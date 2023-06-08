00092302a3bff4c139ec4d9e7f788cdf2dbf3036
Javier Diaz <javier@picnic.pe>
2023-06-08 13:49:20 -0500

    cambios taza

--- src/index.ts
+++ src/index.ts
@@ -2,59 +2,54 @@ import {
     ViewerApp,
     AssetManagerPlugin,
     GBufferPlugin,
-    timeout,
-    ProgressivePlugin,
     TonemapPlugin,
+    ProgressivePlugin,
     SSRPlugin,
     SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
     BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-    GammaCorrectionPlugin,
-
     addBasePlugins,
-    ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
-
+   TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
     IViewerPlugin,
 
     // Color, // Import THREE.js internals
     // Texture, // Import THREE.js internals
 } from "webgi";
 import "./styles.css";
+import gsap from "gsap"
+import {ScrollTrigger} from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
+
 
 async function setupViewer(){
 
     // Initialize the viewer
     const viewer = new ViewerApp({
         canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+        useRgbm: false
     })
 
+  
     // Add some plugins
     const manager = await viewer.addPlugin(AssetManagerPlugin)
+    const camera = viewer.scene.activeCamera
+    const position = camera.position
+    const target = camera.target
+
 
     // Add a popup(in HTML) with download progress when any asset is downloading.
     await viewer.addPlugin(AssetManagerBasicPopupPlugin)
+    
+   
+    //await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+   
+    await viewer.addPlugin(SSRPlugin)
+    await viewer.addPlugin(SSAOPlugin)
+   
+    await viewer.addPlugin(BloomPlugin)
 
-    // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
-
-    // or use this to add all main ones at once.
     await addBasePlugins(viewer)
 
     // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
@@ -63,7 +58,7 @@ async function setupViewer(){
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/classic-watch.glb")
+    await manager.addFromPath("./assets/cafe-nuevo2.glb")
 
     // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
@@ -77,6 +72,127 @@ async function setupViewer(){
     // Add plugins to the UI to see their settings.
     uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
 
+
+
+    function setupScrollanimation(){
+        const tl = gsap.timeline()
+
+        //first section
+        tl
+        .to(position,{
+            x: -5.05,
+            y: 5.53,
+            z: -8.30,
+        
+            scrollTrigger:{
+                trigger: ".second",
+                start: "0 bottom",
+               
+                end: "top top",
+                scrub: 1,
+                immediateRender: false
+        },onUpdate})
+
+        .to(".section--one--container",{
+            opacity: 0,
+            yPercent: '-150',
+            scrollTrigger:{
+                trigger: ".second",
+                start: "top bottom",
+                end: "top 80%",
+                scrub: 1,
+                immediateRender: false
+        }})
+
+        .to(".logo",{
+            opacity: 1,
+            y: 104,
+            scrollTrigger:{
+                trigger: ".second",
+                start: "top bottom",
+                
+                end: "top 80%",
+                scrub: 1,
+                immediateRender: false
+        }})
+
+
+        .to(target,{
+            x: -2.27,
+            y: -0.29,
+            z: 1.04,
+        
+            scrollTrigger:{
+                trigger: ".second",
+                start: "top bottom",
+                
+                end: "top top",
+                scrub: 1,
+                immediateRender: false
+        }})
+
+
+        .to(position,{
+            x: 3.32,
+            y: 4.47,
+            z: 5.56,
+        
+            scrollTrigger:{
+                trigger: ".third",
+                start: "top bottom",
+               
+                end: "top top",
+                scrub: 1,
+                immediateRender: false
+        },onUpdate})
+
+        .to(".section--two--container",{
+            opacity: 0,
+            yPercent: '-150',
+            scrollTrigger:{
+                trigger: ".third",
+                start: "top bottom",
+                end: "top 80%",
+                scrub: 1,
+                immediateRender: false
+        }})
+
+        .to(target,{
+            x: -2.14,
+            y: 0.63,
+            z: 0.17,
+        
+            scrollTrigger:{
+                trigger: ".third",
+                start: "top bottom",
+                
+                end: "top top",
+                scrub: 1,
+                immediateRender: false
+        }})
+       
+    }
+
+    setupScrollanimation()
+
+
+    //WEBGI UPDATE
+
+    let needsUpdate = true;
+
+    function onUpdate(){
+        needsUpdate = true
+        viewer.renderer.resetShadows()
+    }
+
+    viewer.addEventListener('preFrame',()=>{
+        if(needsUpdate){
+            camera.positionUpdated(false)
+            camera.targetUpdated(false)
+            needsUpdate = false
+        }
+    })
 }
+
 
 setupViewer()
